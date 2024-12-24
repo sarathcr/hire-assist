375452647d368f950e9839e53ebb44bb3df0b13d
abhinaya0580 <abhinayaskdu@DESKTOP-NQ37QK7>
2024-12-24 17:15:18 +0530

    added Storybook

--- src/app/shared/enum/enum.ts
+++ src/app/shared/enum/enum.ts
@@ -3,3 +3,10 @@ export enum PageLayout {
   // UnAuthorized = 'unauthorized', // Key for unauthorized layout
   // Error = 'error' // Key for error layout
 }
+
+export enum ButtonType {
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Tertiary = 'tertiary'
+}
+
