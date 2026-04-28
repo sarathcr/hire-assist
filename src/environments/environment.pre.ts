0a72062d276575e843aae14c9ed39227e5bca43b
scheerakkadan <scheerakkadan@opentrends.net>
2026-04-28 12:21:12 +0530

    env changes

--- src/environments/environment.pre.ts
+++ src/environments/environment.pre.ts
@@ -1,12 +1,35 @@
+const hostname = typeof window !== 'undefined' ? window.location.hostname : '';
+
 export const environment = {
-  isProduction: false,
+  // Production is true only for the main production domain
+  isProduction:
+    hostname.includes('hire-assist.vercel.app') &&
+    !hostname.includes('prehire-assist'),
+
   baseUrl: 'https://localhost:7238',
-  authorizationUrl: 'https://prehireassistsecurity.runasp.net',
-  assessmentUrl: 'https://prehireassistassessment.runasp.net',
-  collectionUrl: 'https://prehireassistcollection.runasp.net',
-  intreviewUrl: 'https://prehireassistinterview.runasp.net',
-  // authorizationUrl: 'https://hireassistsecurity.azurewebsites.net',
-  // assessmentUrl: 'https://hireassistassessment.azurewebsites.net',
-  // collectionUrl: 'https://hireassistcollections.azurewebsites.net',
-  // intreviewUrl: ' https://hireassistinterview.azurewebsites.net',
+
+  // Dynamic URL Selection
+  authorizationUrl: hostname.includes('prehire-assist')
+    ? 'https://prehireassistsecurity.runasp.net'
+    : hostname.includes('hire-assist.vercel.app')
+      ? 'https://hireassistsecurity.runasp.net'
+      : 'https://devhireassistsecurity.runasp.net',
+
+  assessmentUrl: hostname.includes('prehire-assist')
+    ? 'https://prehireassistassessment.runasp.net'
+    : hostname.includes('hire-assist.vercel.app')
+      ? 'https://hireassistassessment.runasp.net'
+      : 'https://devhireassistassessment.runasp.net',
+
+  collectionUrl: hostname.includes('prehire-assist')
+    ? 'https://prehireassistcollection.runasp.net'
+    : hostname.includes('hire-assist.vercel.app')
+      ? 'https://hireassistcollection.runasp.net'
+      : 'https://devhireassistcollection.runasp.net',
+
+  intreviewUrl: hostname.includes('prehire-assist')
+    ? 'https://prehireassistinterview.runasp.net'
+    : hostname.includes('hire-assist.vercel.app')
+      ? 'https://hireassistinterview.runasp.net'
+      : 'https://devhireassistinterview.runasp.net',
 };
