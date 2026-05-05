0d41dd7b25f54e9b9f99bdd2512401cbf50bf0a3
scheerakkadan <scheerakkadan@opentrends.net>
2026-05-05 17:16:17 +0530

    test pre production deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ const hostname = typeof window !== 'undefined' ? window.location.hostname : '';
 
 export const environment = {
   // Production is true only for the main production domain
+  //test
   isProduction:
     hostname.includes('hire-assist.vercel.app') &&
     !hostname.includes('prehire-assist'),
